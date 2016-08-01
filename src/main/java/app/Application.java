e91df84200b951c6547c9ee29a73d90c302f8e03
jaqen0000 <jaqen0000@gmail.com>
2016-08-01 17:34:46 +0800

    init project

---
+++ src/main/java/app/Application.java
@@ -0,0 +1,20 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Application {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
